2fab0c1b82f87e717560cf563afa6c642f7bc84c
Sergey <fediyaa1@mail.ru>
2022-09-18 13:08:33 +0300

    Communication in chat

--- mybot.js
+++ mybot.js
@@ -2,10 +2,16 @@ const mineflayer = require("mineflayer")
 
 const bot = mineflayer.createBot({
     host: "localhost",
-    port: "1683",
+    port: "6648",
     version: "1.18.2",
     username: "MyBot" })
 
 bot.once('spawn', function () {
     bot.chat('Привет мир!')
+})
+
+bot.on('chat', function (username,message){
+    if(message === "Ты бот?"){
+        bot.chat("Нет! Я не бот")
+    }
 })
