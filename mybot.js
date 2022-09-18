3bf075a102b945c6b3a06f021b7c8ddb4725bcb6
Sergey <fediyaa1@mail.ru>
2022-09-18 13:21:21 +0300

    Comments

--- mybot.js
+++ mybot.js
@@ -1,15 +1,18 @@
 const mineflayer = require("mineflayer")
 
+// Подключение бота к серверу
 const bot = mineflayer.createBot({
     host: "localhost",
     port: "6648",
     version: "1.18.2",
     username: "MyBot" })
 
+// Вывод сообщения в чат при входе на сервер
 bot.once('spawn', function () {
     bot.chat('Привет мир!')
 })
 
+// Ответ на сообщения в чате
 bot.on('chat', function (username,message){
     if(message === "Ты бот?"){
         bot.chat("Нет! Я не бот")
