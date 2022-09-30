f95ffd7803928bb78c4243396d92aed03b314675
Sergey <fediyaa1@mail.ru>
2022-09-30 15:45:42 +0300

    Download functional

--- mybot.js
+++ mybot.js
@@ -3,18 +3,22 @@ const mineflayer = require("mineflayer")
 // Подключение бота к серверу
 const bot = mineflayer.createBot({
     host: "localhost",
-    port: "1354",
+    port: "23155",
     version: "1.18.2",
     username: "MyBot" })
 
     // Вывод сообщения в чат при входе на сервер
     bot.once('spawn', function () {
         bot.chat('Привет мир!');
-});
-
-    // Ответ на сообщения в чате CTR + C (Перезагрузить бота)
-        bot.on('chat', function Hi (username,message) {
-            if(message === "Ты бот?" && username ==="Bebra_2005") {
-        setTimeout(() => bot.chat(username + " , нет я не бот"), 5000);
-    }
     });
+
+
+    bot.on('chat', function Hi (username,message) {
+        if(username === "MyBot") return;
+          if (message === "Ты бот?" && username === "Sergey") {
+              setTimeout(() => bot.chat(username + " , нет я не бот"), 5000);
+          } else {
+              setTimeout(() => bot.chat(username + " , я тебя не знаю"), 5000);
+          }
+    });
+
