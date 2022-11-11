a6f06a1aa3149501a3d55c8f85b7dc832b67898f
Sergey <fediyaa1@mail.ru>
2022-11-11 17:30:44 +0300

    download food

--- mybot.js
+++ mybot.js
@@ -6,7 +6,7 @@ const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathf
 // Подключение бота к серверу
 const bot = mineflayer.createBot({
     host: "localhost",
-    port: "26692",
+    port: "20225",
     version: "1.18.2",
     username: "MyBot" });
 
@@ -125,26 +125,35 @@ const bot = mineflayer.createBot({
 //---------------------------------------------------------------------
 
 // Взаимодействие с коровами
-bot.on('spawn', async function() {
-    await startMilking()
-    async function startMilking() {
-        setTimeout(async function() {
+ bot.on('spawn', async function() {
+     await startMilking()
+     async function startMilking() {
+         setTimeout(async function() {
 
-            let nearbyCows = Object.values(bot.entities).filter(e => e.name == 'cow' && e.position.distanceTo(bot.entity.position) < 5
-                && !e.metadata[16]);
+             let nearbyCows = Object.values(bot.entities).filter(e => e.name == 'cow' && e.position.distanceTo(bot.entity.position) < 5
+               && !e.metadata[16]);
 
-            let cow = nearbyCows[Math.floor(Math.random() * nearbyCows.length)];
+             let cow = nearbyCows[Math.floor(Math.random() * nearbyCows.length)];
 
             await bot.lookAt(cow.position, false)
 
-            let empty_bucket = bot.inventory.items().filter(item => item.name == 'bucket')[0];
+             let empty_bucket = bot.inventory.items().filter(item => item.name == 'bucket')[0];
 
-           /* if(!empty_bucket) {
+            /* if(!empty_bucket) {
                 console.log('[Предупреждение] В моем инвентаре нет пустого ведра');
                 return startMilking(); */ // По желанию вывовод в консоль
 
-            await startMilking();
-        }, 2000)
-    }
+             await startMilking();
+   }, 2000)
+ }
 });
 //---------------------------------------------------------------------
+// Считаем значения Здоровья и Еды
+bot.on('chat', function (username,message){
+    if(message === "Здоровье"){
+        bot.chat('У меня ' + bot.health.toFixed(0) + ' здоровье')
+    }
+    if(message === "Еда"){
+        bot.chat('У меня ' + bot.food + ' еды')
+    }
+})
